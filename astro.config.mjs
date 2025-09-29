44e90b83ac1bb89771b8d4358e766407a5d972bf
punkuz-bit <punkuz@gmail.com>
2025-09-29 08:50:08 -0400

    Create astro.config.mjs

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+js
+
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
+
+export default defineConfig({
+  integrations: [mdx(), tailwind()],
+  site: 'https://pankajmisra.com'
+});
