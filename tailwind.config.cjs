2411c18b54c341734c42dcbc3a5fc89906f0cf89
punkuz-bit <punkuz@gmail.com>
2025-09-29 08:50:50 -0400

    Create tailwind.config.cjs

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./src/**/*.{astro,md,mdx,js,ts}','./content/**/*.{md,mdx}'],
+  theme: {
+    extend: {
+      colors:{
+        obsidian:'#0b0d16',
+        indigo:'#1a237e',
+        gold:'#C9A227'
+      }
+    }
+  },
+  plugins: []
+}
